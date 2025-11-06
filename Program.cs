6fb557d022733135ce6078dc0d938889be7e10eb
Ковалев Максим <kovalev_mk@directum.ru>
2025-11-06 13:09:39 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿using Npgsql;
+using ClosedXML.Excel;
+using StatisticSelect;
+
+class Program
+{
+  static void Main()
+  {
+    Console.WriteLine("Скрипт запущен");
+    var workbook = new XLWorkbook();
+    var sheet = workbook.Worksheets.Add("Statistic");
+    sheet.Columns().Width = 30;
+
+    var dbList = GetDbList();
+    foreach (var db in dbList)
+    {
+      Console.WriteLine(db);
+      GetStatsticToFile(db, workbook);
+    }
+    workbook.SaveAs("./statistic.xlsx");
+  }
+
+  private static void GetStatsticToFile(string dbName, XLWorkbook book)
+  {
+    var statisticSheet = book.Worksheets.Where(x => x.Name == "Statistic").First();
+    var connectionString = GetConnectionString(dbName);
+    using (var conn = new NpgsqlConnection(connectionString))
+    {
+      conn.Open();
+      using (var cmd = new NpgsqlCommand(Variables.SqlStatistic, conn))
+      {
+        try
+        {
+          using (var reader = cmd.ExecuteReader())
+          {
+            while (reader.Read())
+            {
+              if (statisticSheet.LastRowUsed() == null)
+              {
+                FillColumnNames(reader, statisticSheet);
+              }
+
+              var row = statisticSheet.LastRowUsed()!.RowNumber() + 1;
+              for (int col = 0; col < reader.FieldCount; col++)
+              {
+                statisticSheet.Cell(row, col + 1).Value = reader[col].ToString();
+              }
+            }
+          }
+        }
+        catch (PostgresException ex)
+        {
+          Console.WriteLine($"Error on DB \"{dbName}\": {ex.Message}");
+          var errorSheet = book.Worksheets.Where(x => x.Name == "Error db names").FirstOrDefault();
+          var row = 1;
+          if (errorSheet == null)
+          {
+            errorSheet = book.Worksheets.Add("Error db names");
+            errorSheet.ColumnWidth = 30;
+          }
+          else
+            row = errorSheet.LastRowUsed()!.RowNumber() + 1;
+
+          errorSheet.Cell(row, 1).Value = dbName;
+          errorSheet.Cell(row, 2).Value = ex.Message;
+        }
+      }
+    }
+  }
+
+  private static void FillColumnNames(NpgsqlDataReader reader, IXLWorksheet sheet)
+  {
+    for (int i = 0; i < reader.FieldCount; i++)
+    {
+      string columnName = reader.GetName(i);
+      sheet.Cell(1, i + 1).Value = columnName;
+    }
+  }
+
+  private static IEnumerable<string> GetDbList()
+  {
+    string sqlDbList = @"SELECT datname FROM pg_database WHERE datistemplate = false;";
+    var connectionString = GetConnectionString(Variables.SystemDBName);
+    using (var conn = new NpgsqlConnection(connectionString))
+    {
+      conn.Open();
+      using (var cmd = new NpgsqlCommand(sqlDbList, conn))
+      {
+        using (var reader = cmd.ExecuteReader())
+        {
+          while (reader.Read())
+          {
+            var rowValues = new List<string>();
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+              rowValues.Add(reader[i].ToString());
+            }
+            yield return string.Join("; ", rowValues);
+          }
+        }
+      }
+    }
+  }
+
+  private static string GetConnectionString(string dbName)
+  {
+    var sb = new NpgsqlConnectionStringBuilder();
+    sb.Host = "teleport.directum24.ru";
+    sb.Port = 443;
+    sb.Username = "adviser";
+    sb.Password = "YUTebh7263ukg%$^yjthgff";
+    sb.SslMode = SslMode.VerifyFull;
+    sb.RootCertificate = Variables.SSLRootCert;
+    sb.SslKey = Variables.SSLKey;
+    sb.SslCertificate = Variables.SSLCert;
+    sb.Database = dbName;
+    return sb.ConnectionString;
+  }
+}
---
+++ Variables.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatisticSelect
+{
+  public static class Variables
+  {
+    public const string SqlStatistic = @"
+            WITH
+            total_counts AS (
+                SELECT
+                    SUM(CASE WHEN cp.discriminator = '593e143c-616c-4d95-9457-fd916c4aa7f8' THEN 1 ELSE 0 END) AS total_organizations,
+                    SUM(CASE WHEN cp.discriminator = '80c4e311-e95f-449b-984d-1fd540b8f0af' THEN 1 ELSE 0 END) AS total_banks
+                FROM sungero_parties_counterparty cp
+            ),
+            reliability_counts AS (
+                SELECT
+                    SUM(CASE WHEN discriminator = '593e143c-616c-4d95-9457-fd916c4aa7f8' AND reliability = 'High' THEN 1 ELSE 0 END) AS org_high,
+                    SUM(CASE WHEN discriminator = '593e143c-616c-4d95-9457-fd916c4aa7f8' AND reliability = 'Medium' THEN 1 ELSE 0 END) AS org_medium,
+                    SUM(CASE WHEN discriminator = '593e143c-616c-4d95-9457-fd916c4aa7f8' AND reliability = 'Low' THEN 1 ELSE 0 END) AS org_low,
+                    SUM(CASE WHEN discriminator = '80c4e311-e95f-449b-984d-1fd540b8f0af' AND reliability = 'High' THEN 1 ELSE 0 END) AS bank_high,
+                    SUM(CASE WHEN discriminator = '80c4e311-e95f-449b-984d-1fd540b8f0af' AND reliability = 'Medium' THEN 1 ELSE 0 END) AS bank_medium,
+                    SUM(CASE WHEN discriminator = '80c4e311-e95f-449b-984d-1fd540b8f0af' AND reliability = 'Low' THEN 1 ELSE 0 END) AS bank_low
+                FROM sungero_parties_counterparty
+            )
+            SELECT
+                tc.total_organizations,
+                tc.total_banks,
+                rc.org_high,
+                rc.org_medium,
+                rc.org_low,
+                rc.bank_high,
+                rc.bank_medium,
+                rc.bank_low,
+                ROUND((rc.org_high::numeric / NULLIF(tc.total_organizations, 0)) * 100, 2) AS org_high_percent,
+                ROUND((rc.org_medium::numeric / NULLIF(tc.total_organizations, 0)) * 100, 2) AS org_medium_percent,
+                ROUND((rc.org_low::numeric / NULLIF(tc.total_organizations, 0)) * 100, 2) AS org_low_percent,
+                ROUND((rc.bank_high::numeric / NULLIF(tc.total_banks, 0)) * 100, 2) AS bank_high_percent,
+                ROUND((rc.bank_medium::numeric / NULLIF(tc.total_banks, 0)) * 100, 2) AS bank_medium_percent,
+                ROUND((rc.bank_low::numeric / NULLIF(tc.total_banks, 0)) * 100, 2) AS bank_low_percent
+            FROM total_counts tc, reliability_counts rc;";
+
+    public const string SystemDBName = "DirRX";
+
+    public const string SSLRootCert = @"C:\Users\kovalev_mk\.tsh\keys\teleport.directum24.ru\cas\tele.example.com.pem";
+    public const string SSLCert = @"C:\Users\kovalev_mk\.tsh\keys\teleport.directum24.ru\kovalev_mk-db\tele.example.com\mt02-ub-x509.pem";
+    public const string SSLKey = @"C:\Users\kovalev_mk\.tsh\keys\teleport.directum24.ru\kovalev_mk";
+  }
+}
